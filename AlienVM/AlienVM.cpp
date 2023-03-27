47c91303c1500fa9b66f350cd4c969ef3cf0af78
shitcoder <otabek.coder@gmail.com>
2023-03-27 14:18:05 +0500

    Refactoring

--- AlienVM/AlienVM.cpp
+++ AlienVM/AlienVM.cpp
@@ -7,7 +7,7 @@ struct VM // size should be 0xa8 == 42x int8
 public:
 	int16_t PC;			// at 0 in struct // type ?
 	int16_t AX;			// at 1 in struct // type ? // AX?
-	uint32_t* data;		// at 2 in struct // type (byte *) // 128 B
+	uint32_t* MEM;		// at 2 in struct // type (byte *) // 128 B
 
 	int8_t EXIT;		// at 4 in struct // type char // FLAG?
 
@@ -32,7 +32,7 @@ public:
 		vm.PC = 0;
 		vm.AX = 0;
 		vm.EXIT = 0;
-		vm.data = (uint32_t*)calloc(0x80, 1);
+		vm.MEM = (uint32_t*)calloc(0x80, 1);
 		vm.ZF = 0;
 		vm.SP = 0;
 		vm.COMMANDS = (uint8_t*)calloc(0x10000, 1); // 0x10000 * 1 = 65536 = 64 KB
@@ -200,7 +200,7 @@ private:
 	void debug()
 	{
 		cout << "debug >";
-		getchar();
+		int x = getchar();
 	}
 
 	void status()
@@ -212,7 +212,7 @@ private:
 		{
 			if (i % 16 == 0)
 				printf("0x%02x: ", i);
-			printf("%04x%s", vm.data[i], ((i + 1) % 16 == 0 ? "\n" : " "));
+			printf("%04x%s", vm.MEM[i], ((i + 1) % 16 == 0 ? "\n" : " "));
 		}
 
 		printf("\n--------------------------------------- Stack ---------------------------------------\n");
@@ -254,7 +254,7 @@ private:
 		case 0x01:
 			sprintf(decoded, "ADDI\tMEM[%02x]  %02x  ->  MEM[%02x]\n\t\t   ADDI\t%02x\t %02x  ->  MEM[%02x]",
 				vm.COMMANDS[vm.PC + 2], vm.COMMANDS[vm.PC + 3], vm.COMMANDS[vm.PC + 1],
-				vm.data[vm.COMMANDS[vm.PC + 2]], vm.COMMANDS[vm.PC + 3], vm.COMMANDS[vm.PC + 1]);
+				vm.MEM[vm.COMMANDS[vm.PC + 2]], vm.COMMANDS[vm.PC + 3], vm.COMMANDS[vm.PC + 1]);
 			break;
 
 		case 0x09:
@@ -267,7 +267,7 @@ private:
 			break;
 
 		case 0x0a:
-			sprintf(decoded, "PUSH\tMEM[%02x]\n\t\t   PUSH\t%02x", vm.COMMANDS[vm.PC + 1], vm.data[vm.COMMANDS[vm.PC + 1]]);
+			sprintf(decoded, "PUSH\tMEM[%02x]\n\t\t   PUSH\t%02x", vm.COMMANDS[vm.PC + 1], vm.MEM[vm.COMMANDS[vm.PC + 1]]);
 			break;
 
 		case 0x0b:
@@ -290,44 +290,44 @@ private:
 		case 0x11:
 			sprintf(decoded, "JE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JE\t%02x\t %02x\t  ->  CODE[%02x]",
 				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
-				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+				vm.MEM[vm.COMMANDS[vm.PC + 1]], vm.MEM[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
 			break;
 
 		case 0x12:
 			sprintf(decoded, "JNE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JNE\t%02x\t %02x\t  ->  CODE[%02x]",
 				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
-				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+				vm.MEM[vm.COMMANDS[vm.PC + 1]], vm.MEM[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
 			break;
 
 		case 0x13:
 			sprintf(decoded, "JLE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JLE\t%02x\t %02x\t  ->  CODE[%02x]",
 				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
-				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+				vm.MEM[vm.COMMANDS[vm.PC + 1]], vm.MEM[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
 			break;
 
 		case 0x14:
 			sprintf(decoded, "JGE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JGE\t%02x\t %02x\t  ->  CODE[%02x]",
 				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
-				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+				vm.MEM[vm.COMMANDS[vm.PC + 1]], vm.MEM[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
 			break;
 
 		case 0x15:
 			sprintf(decoded, "XOR\tMEM[%02x]  MEM[%02x]  ->   MEM[%02x]\n\t\t   XOR\t%02x\t %02x\t  ->   MEM[%02x]",
 				vm.COMMANDS[vm.PC + 2], vm.COMMANDS[vm.PC + 3],
-				vm.COMMANDS[vm.PC + 1], vm.data[vm.COMMANDS[vm.PC + 2]], vm.data[vm.COMMANDS[vm.PC + 3]], vm.COMMANDS[vm.PC + 1]);
+				vm.COMMANDS[vm.PC + 1], vm.MEM[vm.COMMANDS[vm.PC + 2]], vm.MEM[vm.COMMANDS[vm.PC + 3]], vm.COMMANDS[vm.PC + 1]);
 			break;
 
 		case 0x16:
 			sprintf(decoded, "STORE\tCODE[MEM[%02x]]  MEM[%02x]\n\t\t   STORE\tCODE[%02x]      %02x",
-				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.PC + 2], *(int32_t*)(vm.data + vm.COMMANDS[vm.PC + 1]),
-				vm.data[vm.COMMANDS[vm.PC + 2]]);
+				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.PC + 2], *(int32_t*)(vm.MEM + vm.COMMANDS[vm.PC + 1]),
+				vm.MEM[vm.COMMANDS[vm.PC + 2]]);
 			break;
 
 		case 0x17:
 			sprintf(decoded, "LOAD\tMEM[%02x]  CODE[MEM[%02x]]\n\t\t   LOAD\tMEM[%02x]\t CODE[%02x]\n\t\t   LOAD\tMEM[%02x]  %02x",
 				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.PC + 2],
-				vm.COMMANDS[vm.PC + 1], vm.data[vm.COMMANDS[vm.PC + 2]],
-				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.data[vm.COMMANDS[vm.PC + 2]]]);
+				vm.COMMANDS[vm.PC + 1], vm.MEM[vm.COMMANDS[vm.PC + 2]],
+				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.MEM[vm.COMMANDS[vm.PC + 2]]]);
 			break;
 
 		case 0x18:
@@ -345,14 +345,14 @@ private:
 
 	void add() // 0x00
 	{
-		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.COMMANDS[vm.PC + 2] + vm.COMMANDS[vm.PC + 3];
+		vm.MEM[vm.COMMANDS[vm.PC + 1]] = vm.COMMANDS[vm.PC + 2] + vm.COMMANDS[vm.PC + 3];
 
 		vm.PC += 6;
 	}
 
 	void addi() // 0x01
 	{
-		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.data[vm.COMMANDS[vm.PC + 2]] + vm.COMMANDS[vm.PC + 3];
+		vm.MEM[vm.COMMANDS[vm.PC + 1]] = vm.MEM[vm.COMMANDS[vm.PC + 2]] + vm.COMMANDS[vm.PC + 3];
 
 		vm.PC += 6;
 	}
@@ -397,7 +397,7 @@ private:
 
 	void jmp() // 0x08
 	{
-		vm.PC = vm.data[vm.COMMANDS[vm.PC + 2]];
+		vm.PC = vm.MEM[vm.COMMANDS[vm.PC + 2]];
 		vm.PC += 6;
 	}
 
@@ -410,8 +410,7 @@ private:
 		int8_t syscall_no = vm.COMMANDS[vm.PC + 1];
 		int8_t argc = vm.COMMANDS[vm.PC + 2];
 
-		uint32_t arg1 = 0, arg2 = 0, arg3 = 0;
-		int64_t result = 0;
+		uint32_t arg1 = 0, arg2 = 0, arg3 = 0, result = 0;
 
 		if (argc > 0)
 			arg1 = vm.STACK[--vm.SP];
@@ -430,23 +429,23 @@ private:
 
 	void push() // 0x0a
 	{
-		vm.STACK[vm.SP++] = vm.data[vm.COMMANDS[vm.PC + 1]];
+		vm.STACK[vm.SP++] = vm.MEM[vm.COMMANDS[vm.PC + 1]];
 
 		vm.PC += 6;
 	}
 
 	void pop() // 0x0b
 	{
-		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.STACK[--vm.SP];
+		vm.MEM[vm.COMMANDS[vm.PC + 1]] = vm.STACK[--vm.SP];
 
 		vm.PC += 6;
 	}
 
 	void mov() // 0x0c
 	{
-		//vm.data[vm.COMMANDS[vm.PC + 1]] = (uint32_t)vm.COMMANDS[vm.PC + 2];
+		//vm.MEM[vm.COMMANDS[vm.PC + 1]] = (uint32_t)vm.COMMANDS[vm.PC + 2];
 
-		vm.data[vm.COMMANDS[vm.PC + 1]] = *(uint32_t*)(vm.COMMANDS + vm.PC + 2);
+		vm.MEM[vm.COMMANDS[vm.PC + 1]] = *(uint32_t*)(vm.COMMANDS + vm.PC + 2);
 
 		vm.PC += 6;
 	}
@@ -476,7 +475,7 @@ private:
 
 	void je() // 0x11
 	{
-		if (vm.data[vm.COMMANDS[vm.PC + 1]] == vm.data[vm.COMMANDS[vm.PC + 2]])
+		if (vm.MEM[vm.COMMANDS[vm.PC + 1]] == vm.MEM[vm.COMMANDS[vm.PC + 2]])
 		{
 			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
 		}
@@ -486,7 +485,7 @@ private:
 
 	void jne() // 0x12
 	{
-		if (vm.data[vm.COMMANDS[vm.PC + 1]] != vm.data[vm.COMMANDS[vm.PC + 2]])
+		if (vm.MEM[vm.COMMANDS[vm.PC + 1]] != vm.MEM[vm.COMMANDS[vm.PC + 2]])
 		{
 			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
 		}
@@ -496,7 +495,7 @@ private:
 
 	void jle() // 0x13
 	{
-		if (vm.data[vm.COMMANDS[vm.PC + 1]] <= vm.data[vm.COMMANDS[vm.PC + 2]])
+		if (vm.MEM[vm.COMMANDS[vm.PC + 1]] <= vm.MEM[vm.COMMANDS[vm.PC + 2]])
 		{
 			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
 		}
@@ -506,7 +505,7 @@ private:
 
 	void jge() // 0x14
 	{
-		if (vm.data[vm.COMMANDS[vm.PC + 1]] >= vm.data[vm.COMMANDS[vm.PC + 2]])
+		if (vm.MEM[vm.COMMANDS[vm.PC + 1]] >= vm.MEM[vm.COMMANDS[vm.PC + 2]])
 		{
 			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
 		}
@@ -516,21 +515,21 @@ private:
 
 	void xorr() // 0x15
 	{
-		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.data[vm.COMMANDS[vm.PC + 2]] ^ vm.data[vm.COMMANDS[vm.PC + 3]];
+		vm.MEM[vm.COMMANDS[vm.PC + 1]] = vm.MEM[vm.COMMANDS[vm.PC + 2]] ^ vm.MEM[vm.COMMANDS[vm.PC + 3]];
 
 		vm.PC += 6;
 	}
 
 	void store() // 0x16
 	{
-		vm.COMMANDS[*(int32_t*)(vm.data + vm.COMMANDS[vm.PC + 1])] = vm.data[vm.COMMANDS[vm.PC + 2]];
+		vm.COMMANDS[*(int32_t*)(vm.MEM + vm.COMMANDS[vm.PC + 1])] = vm.MEM[vm.COMMANDS[vm.PC + 2]];
 
 		vm.PC += 6;
 	}
 
 	void load() // 0x17
 	{
-		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.COMMANDS[*(int32_t*)(vm.data + vm.COMMANDS[vm.PC + 2])];
+		vm.MEM[vm.COMMANDS[vm.PC + 1]] = vm.COMMANDS[*(int32_t*)(vm.MEM + vm.COMMANDS[vm.PC + 2])];
 
 		vm.PC += 6;
 	}
@@ -541,7 +540,7 @@ private:
 		int x = getchar();
 		//unsigned char x = 0x63;
 
-		vm.data[vm.COMMANDS[vm.PC + 1]] = x;
+		vm.MEM[vm.COMMANDS[vm.PC + 1]] = x;
 
 		vm.PC += 6;
 	}
