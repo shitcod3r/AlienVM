49abcb68d27b4f0e7080fbbd8d4695ddd0b1ec9f
shitcoder <otabek.coder@gmail.com>
2023-03-27 10:57:15 +0500

    Add project files.

---
+++ AlienVM/AlienVM.cpp
@@ -0,0 +1,560 @@
+#include <iostream>
+
+using namespace std;
+
+struct VM // size should be 0xa8 == 42x int8
+{
+public:
+	int16_t PC;			// at 0 in struct // type ?
+	int16_t AX;			// at 1 in struct // type ? // AX?
+	uint32_t* data;		// at 2 in struct // type (byte *) // 128 B
+
+	int8_t EXIT;		// at 4 in struct // type char // FLAG?
+	int16_t ZF;			// at 0x22 in struct
+	uint8_t* COMMANDS;	// at 0x24 in struct // 64 KB
+	uint32_t* STACK;	// at 0x26 in struct // 2 KB
+	int16_t SP;			// at 0x28 in struct // type ? // SP
+};
+
+class AlienVirtualMachine
+{
+private:
+	VM vm;
+	const char* keycode = "c0d3_r3d_5hutd0wn";
+	bool debuging = false;
+
+public:
+
+	AlienVirtualMachine(void* _commands, size_t _commands_size)
+	{
+		vm.PC = 0;
+		vm.AX = 0;
+		vm.EXIT = 0;
+		vm.data = (uint32_t*)calloc(0x80, 1);
+		vm.ZF = 0;
+		vm.SP = 0;
+		vm.COMMANDS = (uint8_t*)calloc(0x10000, 1); // 0x10000 * 1 = 65536 = 64 KB
+
+		if (vm.COMMANDS == NULL)
+		{
+			cerr << "Couldn't allocate memory for uint8_t* COMMANDS" << endl;
+			exit(-1);
+		}
+
+		// skiping first 3 - 'UwU' bytes of the file
+		memcpy(vm.COMMANDS, (uint8_t*)_commands + 3, _commands_size - 3);
+
+
+		vm.STACK = (uint32_t*)calloc(0x200, 4); // 0x200 * 4 = 2048 = 2 KB
+
+
+		if (vm.STACK == NULL)
+		{
+			cerr << "Couldn't allocate memory for uint32_t* STACK" << endl;
+			exit(-1);
+		}
+	}
+
+	void run()
+	{
+		while (vm.EXIT == 0)
+		{
+			step();
+		}
+
+		cout << "VM has finished" << endl;
+	}
+
+private:
+
+	void step()
+	{
+		if (vm.PC == 0x1f2)
+		{
+			debuging = true;
+		}
+
+		if (debuging)
+		{
+			debug();
+		}
+
+		status();
+
+		if (vm.COMMANDS[vm.PC] > 0x19)
+		{
+			cout << "dead" << endl;
+			exit(0);
+		}
+
+		switch (vm.COMMANDS[vm.PC])
+		{
+			//case 0x00:
+			//	add();
+			//	break;
+
+		case 0x01:
+			addi();
+			break;
+
+			//case 0x02:
+			//	sub();
+			//	break;
+
+			//case 0x03:
+			//	subi();
+			//	break;
+
+			//case 0x04:
+			//	mul();
+			//	break;
+
+			//case 0x05:
+			//	muli();
+			//	break;
+
+			//case 0x06:
+			//	div();
+			//	break;
+
+			//case 0x07:
+			//	cmp();
+			//	break;
+
+			//case 0x08:
+			//	jmp();
+			//	break;
+
+		case 0x09:
+			inv();
+			break;
+
+		case 0x0a:
+			push();
+			break;
+
+			//case 0x0b:
+			//	pop();
+			//	break;
+
+		case 0x0c:
+			mov();
+			break;
+
+			//case 0x0d:
+			//	nop();
+			//	break;
+
+		case 0x0e:
+			exitt();
+			break;
+
+			//case 0x0f:
+			//	print();
+			//	break;
+
+		case 0x10:
+			putc();
+			break;
+
+		case 0x11:
+			je();
+			break;
+
+		case 0x12:
+			jne();
+			break;
+
+		case 0x13:
+			jle();
+			break;
+
+		case 0x14:
+			jge();
+			break;
+
+		case 0x15:
+			xorr();
+			break;
+
+		case 0x16:
+			store();
+			break;
+
+		case 0x17:
+			load();
+			break;
+
+		case 0x18:
+			input();
+			break;
+
+		default:
+			debuging = true;
+		}
+
+	}
+
+	void debug()
+	{
+		cout << "debug >" << endl;
+		getchar();
+	}
+
+	void status()
+	{
+		//system("cls");
+		printf("\n\n--------------------------------------- Memory --------------------------------------\n");
+		printf("\t 0    1    2    3    4    5    6    7    8    9    a    b    c    d    e    f \n");
+		for (int i = 0; i < 0x80 / 4; i++)
+		{
+			if (i % 16 == 0)
+				printf("0x%02x: ", i);
+			printf("%04x%s", vm.data[i], ((i + 1) % 16 == 0 ? "\n" : " "));
+		}
+
+		printf("\n--------------------------------------- Stack ---------------------------------------\n");
+		printf("\t 0    1    2    3    4    5    6    7    8    9    a    b    c    d    e    f \n");
+		for (int i = 0; i < (0x200 / 4) - 0x60; i++)
+		{
+			if (i % 16 == 0)
+				printf("0x%02x: ", i);
+			printf("%04x%s", vm.STACK[i], ((i + 1) % 16 == 0 ? "\n" : " "));
+		}
+
+		printf("\n-------------------------------------- Registers ------------------------------------\n");
+		printf("PC: 0x%02x\n", vm.PC);
+		printf("ZF: 0x%02x\n", vm.ZF);
+		printf("SP: 0x%02x\n", vm.SP);
+
+		printf("\n---------------------------------------- Code ---------------------------------------\n");
+		for (int i = 0; i < 6; i++)
+		{
+			printf("%02x%s", vm.COMMANDS[vm.PC + i], (i + 1 < 6 ? " " : ": "));
+		}
+		printf("%s\n", decode());
+
+		printf("\n");
+	}
+
+	char* decode()
+	{
+		char* decoded = (char*)calloc(80, 1);
+
+		if (decoded == NULL)
+		{
+			cerr << "Couldn't allocate memory for char* decoded in decode()" << endl;
+			exit(-1);
+		}
+
+		switch (vm.COMMANDS[vm.PC])
+		{
+		case 0x01:
+			sprintf(decoded, "ADDI\tMEM[%02x]  %02x  ->  MEM[%02x]\n\t\t   ADDI\t%02x\t %02x  ->  MEM[%02x]",
+				vm.COMMANDS[vm.PC + 2], vm.COMMANDS[vm.PC + 3], vm.COMMANDS[vm.PC + 1],
+				vm.data[vm.COMMANDS[vm.PC + 2]], vm.COMMANDS[vm.PC + 3], vm.COMMANDS[vm.PC + 1]);
+			break;
+
+		case 0x0a:
+			sprintf(decoded, "PUSH\tMEM[%02x]\n\t\t   PUSH\t%02x", vm.COMMANDS[vm.PC + 1], vm.data[vm.COMMANDS[vm.PC + 1]]);
+			break;
+
+		case 0x0b:
+			sprintf(decoded, "POP\tMEM[%02x]", vm.COMMANDS[vm.PC + 1]);
+			break;
+
+		case 0x0c:
+			sprintf(decoded, "MOV\tMEM[%02x]  %02x",
+				vm.COMMANDS[vm.PC + 1], *(uint32_t*)(vm.COMMANDS + vm.PC + 2));
+			break;
+
+		case 0x0e:
+			sprintf(decoded, "EXIT");
+			break;
+
+		case 0x10:
+			sprintf(decoded, "PUTC\t'%c'", (vm.COMMANDS[vm.PC + 1] != '\n' ? (char)vm.COMMANDS[vm.PC + 1] : '\\n'));
+			break;
+
+		case 0x11:
+			sprintf(decoded, "JE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JE\t%02x\t %02x\t  ->  CODE[%02x]",
+				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
+				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+			break;
+
+		case 0x12:
+			sprintf(decoded, "JNE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JNE\t%02x\t %02x\t  ->  CODE[%02x]",
+				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
+				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+			break;
+
+		case 0x13:
+			sprintf(decoded, "JLE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JLE\t%02x\t %02x\t  ->  CODE[%02x]",
+				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
+				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+			break;
+
+		case 0x14:
+			sprintf(decoded, "JGE\tMEM[%02x]  MEM[%02x]  ->  CODE[%02x]\n\t\t   JGE\t%02x\t %02x\t  ->  CODE[%02x]",
+				vm.COMMANDS[vm.PC + 1], (uint32_t)vm.COMMANDS[vm.PC + 2], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6,
+				vm.data[vm.COMMANDS[vm.PC + 1]], vm.data[vm.COMMANDS[vm.PC + 2]], ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6);
+			break;
+
+		case 0x15:
+			sprintf(decoded, "XOR\tMEM[%02x]  MEM[%02x]  ->   MEM[%02x]\n\t\t   XOR\t%02x\t %02x\t  ->   MEM[%02x]",
+				vm.COMMANDS[vm.PC + 2], vm.COMMANDS[vm.PC + 3],
+				vm.COMMANDS[vm.PC + 1], vm.data[vm.COMMANDS[vm.PC + 2]], vm.data[vm.COMMANDS[vm.PC + 3]], vm.COMMANDS[vm.PC + 1]);
+			break;
+
+		case 0x16:
+			sprintf(decoded, "STORE\tCODE[MEM[%02x]]  MEM[%02x]\n\t\t   STORE\tCODE[%02x]      %02x",
+				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.PC + 2], *(int32_t*)(vm.data + vm.COMMANDS[vm.PC + 1]),
+				vm.data[vm.COMMANDS[vm.PC + 2]]);
+			break;
+
+		case 0x17:
+			sprintf(decoded, "LOAD\tMEM[%02x]  CODE[MEM[%02x]]\n\t\t   LOAD\tMEM[%02x]\t CODE[%02x]\n\t\t   LOAD\tMEM[%02x]  %02x",
+				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.PC + 2],
+				vm.COMMANDS[vm.PC + 1], vm.data[vm.COMMANDS[vm.PC + 2]],
+				vm.COMMANDS[vm.PC + 1], vm.COMMANDS[vm.data[vm.COMMANDS[vm.PC + 2]]]);
+			break;
+
+		case 0x18:
+			sprintf(decoded, "INPUT\tMEM[%02x]  getchar()",
+				vm.COMMANDS[vm.PC + 1]);
+			break;
+
+		default:
+			sprintf(decoded, "??????");
+		}
+
+		return decoded;
+	}
+
+
+	void add() // 0x00
+	{
+		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.COMMANDS[vm.PC + 2] + vm.COMMANDS[vm.PC + 3];
+
+		vm.PC += 6;
+	}
+
+	void addi() // 0x01
+	{
+		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.data[vm.COMMANDS[vm.PC + 2]] + vm.COMMANDS[vm.PC + 3];
+
+		vm.PC += 6;
+	}
+
+	void sub() // 0x02
+	{
+
+	}
+
+	void subi() // 0x03
+	{
+
+	}
+
+	void mul() // 0x04
+	{
+
+	}
+
+	void muli() // 0x05
+	{
+
+	}
+
+	void div() // 0x06
+	{
+
+	}
+
+	void cmp() // 0x07
+	{
+		uint8_t a = vm.COMMANDS[vm.PC + 2];
+		uint8_t b = vm.COMMANDS[vm.PC + 3];
+
+		if (a == b)
+		{
+			vm.ZF = 1;
+		}
+
+		vm.PC += 6;
+	}
+
+	void jmp() // 0x08
+	{
+		vm.PC = vm.data[vm.COMMANDS[vm.PC + 2]];
+		vm.PC += 6;
+	}
+
+	void inv() // 0x09
+	{
+
+
+		vm.PC += 6;
+	}
+
+	void push() // 0x0a
+	{
+		vm.STACK[vm.SP++] = vm.data[vm.COMMANDS[vm.PC + 1]];
+
+		vm.PC += 6;
+	}
+
+	void pop() // 0x0b
+	{
+		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.STACK[--vm.SP];
+
+		vm.PC += 6;
+	}
+
+	void mov() // 0x0c
+	{
+		//vm.data[vm.COMMANDS[vm.PC + 1]] = (uint32_t)vm.COMMANDS[vm.PC + 2];
+
+		vm.data[vm.COMMANDS[vm.PC + 1]] = *(uint32_t*)(vm.COMMANDS + vm.PC + 2);
+
+		vm.PC += 6;
+	}
+
+	void nop() // 0x0d
+	{
+		vm.PC += 6;
+	}
+
+	void exitt() // 0x0e
+	{
+		vm.EXIT = 1;
+
+		vm.PC += 6;
+	}
+
+	void print() // 0x0f
+	{
+
+	}
+
+	void putc() // 0x10
+	{
+		putchar(vm.COMMANDS[vm.PC + 1]);
+		vm.PC += 6;
+	}
+
+	void je() // 0x11
+	{
+		if (vm.data[vm.COMMANDS[vm.PC + 1]] == vm.data[vm.COMMANDS[vm.PC + 2]])
+		{
+			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
+		}
+		else
+			vm.PC += 6;
+	}
+
+	void jne() // 0x12
+	{
+		if (vm.data[vm.COMMANDS[vm.PC + 1]] != vm.data[vm.COMMANDS[vm.PC + 2]])
+		{
+			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
+		}
+		else
+			vm.PC += 6;
+	}
+
+	void jle() // 0x13
+	{
+		if (vm.data[vm.COMMANDS[vm.PC + 1]] <= vm.data[vm.COMMANDS[vm.PC + 2]])
+		{
+			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
+		}
+		else
+			vm.PC += 6;
+	}
+
+	void jge() // 0x14
+	{
+		if (vm.data[vm.COMMANDS[vm.PC + 1]] >= vm.data[vm.COMMANDS[vm.PC + 2]])
+		{
+			vm.PC = ((int16_t)vm.COMMANDS[vm.PC + 3]) * 6;
+		}
+		else
+			vm.PC += 6;
+	}
+
+	void xorr() // 0x15
+	{
+		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.data[vm.COMMANDS[vm.PC + 2]] ^ vm.data[vm.COMMANDS[vm.PC + 3]];
+
+		vm.PC += 6;
+	}
+
+	void store() // 0x16
+	{
+		vm.COMMANDS[*(int32_t*)(vm.data + vm.COMMANDS[vm.PC + 1])] = vm.data[vm.COMMANDS[vm.PC + 2]];
+
+		vm.PC += 6;
+	}
+
+	void load() // 0x17
+	{
+		vm.data[vm.COMMANDS[vm.PC + 1]] = vm.COMMANDS[*(int32_t*)(vm.data + vm.COMMANDS[vm.PC + 2])];
+
+		vm.PC += 6;
+	}
+
+	void input() // 0x18
+	{
+		cout << "> ";
+		int x = getchar();
+		//unsigned char x = 0x63;
+
+		vm.data[vm.COMMANDS[vm.PC + 1]] = x;
+
+		vm.PC += 6;
+	}
+};
+
+
+int main()
+{
+	cout << "Starting VM..." << endl;
+
+	cout << "Opening and reading the command file..." << endl;
+	FILE* bin_file = fopen("bin", "rb");
+
+	if (bin_file == NULL)
+	{
+		cerr << "Couldn't open the command file!" << endl;
+		exit(-1);
+	}
+
+	fseek(bin_file, 0, 2);
+	size_t file_size = ftell(bin_file);
+	cout << "Command file size: " << file_size << endl; // 16387 B
+
+	rewind(bin_file);
+
+	void* commands = malloc(file_size);
+
+	if (commands == NULL)
+	{
+		cerr << "Couldn't allocate memory for reading from the command file" << endl;
+		exit(-1);
+	}
+
+	size_t read_length = fread(commands, 1, file_size, bin_file);
+	cout << "Read " << read_length << " bytes from the file" << endl;
+	fclose(bin_file);
+
+
+	cout << "Creating a Virtual Machine..." << endl;
+	AlienVirtualMachine vm = AlienVirtualMachine(commands, file_size);
+
+	cout << "Running the Virtual Machine..." << endl << endl;
+	vm.run();
+
+
+	return 0;
+}
